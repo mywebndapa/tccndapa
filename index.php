736e479ed25004ae462d2ca0313ae81e51216734
mywebndapa <39356558+mywebndapa@users.noreply.github.com>
2018-05-29 23:10:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+    <title>Data Diri</title>
+  </head>
+  <body>
+  <ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">DATA PRIBADI</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">ALL ABOUT APLIKASI</a>
+  </li>
+</ul>
+<br/>
+<br/>
+  <div class="container">
+    <h1>DATA DIRI</h1>
+	<p>NAMA: <span><b>NDAPA MAKABORANG</b></span></p>
+	<p>TTL: <span><b>SUMBA,31 JANUARI 1985 </b></span></p>
+	<p>ALAMAT: <span><b>KANOMAN</b></span></p>
+  <p>EMAIL: <span><b>makaborangndapa@gmail.com</b></span></p>
+	<p>HOBBY: <span><b>MELUKIS</b></span></p>
+	<p>STATUS: <span><b>SUDAH MENIKAH</b></span></p>
+	<p>PENDIDIKAN: <span><b>STMIK AKAKOM</b></span></p>
+  <p>JURUSAN: <span><b>SISTEM INFORMASI</b></span></p>
+  </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  </body>
+</html>
